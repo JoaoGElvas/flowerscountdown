089f2a13ffe35387c7093be1c5cc4314c7b65615
João Gabriel Elvas Ribeiro <joaog.elvas@gmail.com>
2024-01-13 12:22:17 -0300

    [add] slider functional

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,3 +23,20 @@ var x = setInterval (function(){
   }
 
 },1000);
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 5000)
+
+function nextImage(){
+  count++;
+  if(count>4){
+    count = 1;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+
+}
